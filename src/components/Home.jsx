31dbb285a19d41d1c5a0aebe295bf69a6376b782
cedric karungu <ckarungu921@gmail.com>
2022-07-24 23:24:07 +0200

    feat:render error on fetch data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
+	const [blogs, setBlogs] = useState(null);
+	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
 
 	const handleDelete = (id) => {
 		const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -13,18 +13,29 @@ const Home = () => {
 
 	useEffect(() => {
 		fetch('http://localhost:8080/blogs')
-			.then((response) => response.json())
-			.then((data) => {
-        setBlogs(data);
-        setIsPending(false)  
+			.then((response) => {
+				console.log(response);
+				if (!response.ok) {
+					throw Error('Failed to fetch blog data');
+				}
+				return response.json();
 			})
-			.catch();
-	}, [blogs]);
+			.then((data) => {
+				setBlogs(data);
+				setIsPending(false);
+				setError(null);
+			})
+			.catch((err) => {
+				setError(err.message);
+				setIsPending(false);
+			});
+	}, []);
 
 	return (
 		<div className='home'>
-      <div>
-        {isPending && <div>Loading...</div>}
+			<div>
+				{error && <div className='error'>{error}</div>}
+				{isPending && <div>Loading...</div>}
 				{blogs && <BlogList blogs={blogs} />}
 			</div>
 		</div>
