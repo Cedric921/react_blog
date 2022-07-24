80f16b270d9031505a621453fcfc38ff1deb8309
cedric karungu <ckarungu921@gmail.com>
2022-07-24 21:54:30 +0200

    feat: add useState to change name dynamic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const home = () => {
-  return (
-      <div className="home">
-          <h2>homepage</h2>
-    </div>
-  )
-}
+const Home = () => {
+	const [name, setName] = useState('cedric');
+	const handleClick = () => {
+		setName('vb');
+	};
 
-export default home
+	return (
+		<div className='home'>
+      <h2>homepage</h2>
+      <p>{name}</p>
+			<button onClick={handleClick}>click me</button>
+		</div>
+	);
+};
+
+export default Home;
