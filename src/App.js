eccd9aa9a58c79cf1562f75115133445fed1bdc8
cedric karungu <ckarungu921@gmail.com>
2022-07-25 23:04:44 +0200

    feat: add a route for project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
 	return (
-		<div className='App'>
-			<Navbar />
-			<div className='content'>
-				<Home />
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<div className='content'>
+					<Routes>
+						<Route path='/' element={<Home />} />
+					</Routes>
+				</div>
 			</div>
-		</div>
+		</Router>
 	);
 };
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 		data: blogs,
 		isPending,
 		error,
-	} = useFetch('http://localhost:8080/blogs');
+	} = useFetch('http://localhost:8000/blogs');
 
 	return (
 		<div className='home'>
