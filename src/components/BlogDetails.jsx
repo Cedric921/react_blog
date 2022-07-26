62b6dfbfd00e300f11385123f37f6760a9ca1ef6
cedric karungu <ckarungu921@gmail.com>
2022-07-26 11:05:59 +0200

    style: add style to blog details

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
 
 const BlogDetails = () => {
-    const { id } = useParams();
-  return (
-      <div className="blog-details">
-          <h3>Blog  for { id}</h3>
-    </div>
-  )
-}
+	const { id } = useParams();
+	const {
+		data: blog,
+		error,
+		isPending,
+	} = useFetch(`http://localhost:8080/blogs/${id}`);
+	return (
+		<div className='blog-details'>
+			{isPending && <div> Loading ...</div>}
+			{error && <div>{error}</div>}
+			{blog && (
+				<article>
+          <h2>{blog.title}</h2>
+          <p>Wrotten by {blog.author}</p>
+          <div>
+            {blog.body}
+            
+          </div>
+				</article>
+			)}
+		</div>
+	);
+};
 
 export default BlogDetails;
