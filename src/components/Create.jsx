868f4b6a6fcddba13dc58f65edfe0d3c849ae5d3
cedric karungu <ckarungu921@gmail.com>
2022-07-26 11:48:49 +0200

    feat: add state to create form

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Create = () => {
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+	const [author, setAuthor] = useState('cedric');
 	return (
 		<div className='create'>
 			<h2>Add a new Blog</h2>
 			<form>
 				<label htmlFor='tittle'>Blog title</label>
-				<input type='text' required id='tittle' />
+				<input
+					type='text'
+					required
+					id='tittle'
+					name={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
 				<label htmlFor='body'>Blog body</label>
-				<textarea name='' id='' cols='30' rows='10' required ></textarea>
+				<textarea
+					id='body'
+					required
+					name={body}
+					onChange={(e) => setBody(e.target.value)}
+				></textarea>
 				<label htmlFor='author'>Author</label>
-				<input type='text' name='' id='author' />
+				<input
+					type='text'
+					name={author}
+					onChange={(e) => setAuthor((e.target.id = 'author'))}
+				/>
 				<button type='submit'>Submit</button>
 			</form>
 		</div>
