1ce843f8eb5cfa1c369959fb21b8fc2c4fbf77dc
cedric karungu <ckarungu921@gmail.com>
2022-07-26 12:17:36 +0200

    feat: add redirection after creating blog

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,13 +1,36 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
 	const [title, setTitle] = useState('');
 	const [body, setBody] = useState('');
 	const [author, setAuthor] = useState('cedric');
+	const [isPending, setIsPending] = useState(false);
+	// useNavigate for redirection
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		setIsPending(true);
+
+		const blog = { title, body, author };
+
+		fetch('http://localhost:8080/blogs', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(blog),
+		}).then((data) => {
+			setIsPending(false);
+			console.log('new blog created');
+			navigate(-1);
+		});
+	};
+
 	return (
 		<div className='create'>
 			<h2>Add a new Blog</h2>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label htmlFor='tittle'>Blog title</label>
 				<input
 					type='text'
@@ -27,9 +50,14 @@ const Create = () => {
 				<input
 					type='text'
 					name={author}
-					onChange={(e) => setAuthor((e.target.id = 'author'))}
+					onChange={(e) => setAuthor(e.target.value)}
 				/>
-				<button type='submit'>Submit</button>
+				{!isPending && <button type='submit'>Submit</button>}
+				{isPending && (
+					<button type='submit' disabled>
+						Adding blog ...
+					</button>
+				)}
 			</form>
 		</div>
 	);
