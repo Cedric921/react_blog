ee1a9b575562a5bbf4a756f35760ad073563f162
cedric karungu <ckarungu921@gmail.com>
2022-07-26 12:34:30 +0200

    feat: add 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Create from './components/Create';
 import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
 
 const App = () => {
 	return (
@@ -14,6 +15,7 @@ const App = () => {
 						<Route path='/' exact element={<Home />} />
 						<Route path='/create' element={<Create />} />
 						<Route path='/blogs/:id' element={<BlogDetails />} />
+						<Route path='*' element={<NotFound />} />
 					</Routes>
 				</div>
 			</div>
