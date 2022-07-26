4def331fd0277f4af15c6a5daacd1de9e855de05
cedric karungu <ckarungu921@gmail.com>
2022-07-26 11:23:40 +0200

    style: add style to create blog form

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -4,6 +4,15 @@ const Create = () => {
 	return (
 		<div className='create'>
 			<h2>Add a new Blog</h2>
+			<form>
+				<label htmlFor='tittle'>Blog title</label>
+				<input type='text' required id='tittle' />
+				<label htmlFor='body'>Blog body</label>
+				<textarea name='' id='' cols='30' rows='10' required ></textarea>
+				<label htmlFor='author'>Author</label>
+				<input type='text' name='' id='author' />
+				<button type='submit'>Submit</button>
+			</form>
 		</div>
 	);
 };
